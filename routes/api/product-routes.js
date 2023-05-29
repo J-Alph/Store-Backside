660183406a4ed16941c10e0686aad07cd04eab0c
Alphonse Johnson <a.joh26@gmail.com>
2023-05-28 22:40:37 -0500

    added code to models , products tags and tag files. Updates routes on products routes file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,20 +3,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
+// get all products // find all products
   // be sure to include its associated Category and Tag data
-});
+router.get('/', async (req, res) => {
+ 
+  const productData = await Product.findAll().catch((err) =>{
+    res.json(err);
+  });
+  
+  res.json(productData);
+  });
+
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id',async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
